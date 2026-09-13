1fd13c8223339a1f1cc72388ffafb75ca4844859
Md Faruk Hossen Khan <farukinfo@gmail.com>
2026-09-13 13:12:19 +0200

    Tested the happy path of the TaskService

--- packages/application/tests/TaskService.test.ts
+++ packages/application/tests/TaskService.test.ts
@@ -1,23 +1,79 @@
 import "reflect-metadata";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { TaskService } from "../src/services/TaskService";
 import { Task } from "@task-manager/domain";
 import { MockTaskRepository } from "./mocks/MockTaskRepository";
 
 describe("TaskService", () => {
+  let mockTaskRepository: MockTaskRepository;
+  let taskService: TaskService;
+
+  beforeEach(() => {
+    mockTaskRepository = new MockTaskRepository();
+    taskService = new TaskService(mockTaskRepository);
+  });
+
   it("should return all tasks", async () => {
     const tasks: Task[] = [
       { id: 1, title: "Task 1", completed: false },
       { id: 2, title: "Task 2", completed: true },
     ];
 
-    const mockTaskRepository = new MockTaskRepository();
     mockTaskRepository.getTasks.mockResolvedValue(tasks);
 
-    const taskService = new TaskService(mockTaskRepository);
     const result = await taskService.getTasks();
 
     expect(result).toEqual(tasks);
-    expect(mockTaskRepository.getTasks).toHaveBeenCalledTimes(1);
+    expect(mockTaskRepository.getTasks).toHaveBeenCalledOnce();
+  });
+
+  it("Should return the Task with the supplied task id", async () => {
+    const task: Task = { id: 1, title: "Task 1", completed: true };
+
+    mockTaskRepository.getTaskById.mockResolvedValue(task);
+
+    const result = await taskService.getTaskById(1);
+
+    expect(result).toEqual(task);
+    expect(mockTaskRepository.getTaskById).toHaveBeenCalledOnce();
+    expect(mockTaskRepository.getTaskById).toHaveBeenCalledWith(1);
+  });
+
+  it("Should create task", async () => {
+    const task: Task = { id: 1, title: "Task 1", completed: true };
+
+    mockTaskRepository.createTask.mockResolvedValue(task);
+
+    const result = await taskService.createTask("Task 1");
+
+    expect(result).toEqual(task);
+    expect(mockTaskRepository.createTask).toHaveBeenCalledOnce();
+    expect(mockTaskRepository.createTask).toHaveBeenCalledWith("Task 1");
+  });
+
+  it("Should Update the task", async () => {
+    const task: Task = { id: 1, title: "Updated Task 1", completed: false };
+
+    mockTaskRepository.updateTask.mockResolvedValue(task);
+
+    const result = await taskService.updateTask(1, "Updated Task 1", false);
+
+    expect(result).toEqual(task);
+    expect(mockTaskRepository.updateTask).toHaveBeenCalledOnce();
+    expect(mockTaskRepository.updateTask).toHaveBeenCalledWith(
+      1,
+      "Updated Task 1",
+      false,
+    );
+  });
+
+  it("Should Delete the task", async () => {
+    mockTaskRepository.deleteTask.mockResolvedValue(true);
+
+    const result = await taskService.deleteTask(1);
+
+    expect(result).toEqual(true);
+    expect(mockTaskRepository.deleteTask).toHaveBeenCalledOnce();
+    expect(mockTaskRepository.deleteTask).toHaveBeenCalledWith(1);
   });
 });
